64413d67b863a526a741f5ea8a8c0c6e67357fc9
Lubin <lubin.benoit@gmail.com>
2026-02-11 19:00:30 +0100

    feat: integration of actual data and updating of controllers/routes

--- Projet/backend/src/routes/equipements.js
+++ Projet/backend/src/routes/equipements.js
@@ -5,11 +5,13 @@ const {
   getEquipementById,
   createEquipement,
   updateEquipement,
-  deleteEquipement
+  deleteEquipement,
+  getStats
 } = require('../controllers/equipements');
 
 // Routes CRUD
 router.get('/', getAllEquipements);
+router.get('/stats', getStats); // IMPORTANT: avant /:id pour éviter conflit
 router.get('/:id', getEquipementById);
 router.post('/', createEquipement);
 router.put('/:id', updateEquipement);
